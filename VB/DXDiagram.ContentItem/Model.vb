05c42e9997886ce695f7f94d999b73f9a7a83e91
DevExpressExampleBot <fakeEmail>
2020-06-09 17:52:34 +0300

    Source auto update [skip ci]

--- VB/DXDiagram.ContentItem/Model.vb
+++ VB/DXDiagram.ContentItem/Model.vb
@@ -7,7 +7,7 @@ Imports System.Windows
 Namespace DXDiagram.ContentItem
 	Public Class ButtonItemModel
 		Public Sub ShowMessage()
-		MessageBox.Show("This command has been created by our POCO mechanism based on the ShowMessage method")
+			MessageBox.Show("This command has been created by our POCO mechanism based on the ShowMessage method")
 		End Sub
 	End Class
 End Namespace
